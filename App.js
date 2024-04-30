dd94e9afd54006f2c9b97557bed4e1e6299eae6a
smnchoi <70505699+smnchoi@users.noreply.github.com>
2024-04-30 16:01:10 +1200

    (chore) simple polling fetch example
    brew install fastlane

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    setInterval(() => {
+      const index = Math.floor(Math.random() * 100) % 200;
+      console.log("index", index);
+      fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
+        .then((response) => response.json())
+        .then((json) => setData(JSON.stringify(json)));
+    }, 3000);
+
+    return () => {};
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
